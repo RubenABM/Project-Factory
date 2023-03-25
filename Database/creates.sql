e46ddc02a53b7d33381ec70808ba5985c95e692a
RubenABM <rubenbatistamartins@gmail.com>
2023-03-25 16:43:14 +0000

    db

---
+++ Database/creates.sql
@@ -0,0 +1,16 @@
+create table user (
+					user_id SERIAL not null,
+					user_name VARCHAR(50) not null,	
+                    user_email VARCHAR(50) not null,	
+                    user_password VARCHAR(50) not null,	
+                    user_points VARCHAR(50) not null,						
+					primary key (user_id)	
+);
+
+---------TEST TABLE-------------------------
+
+create table students (
+					stu_id SERIAL not null,
+					stu_phone VARCHAR(30) not null,						
+					primary key (stu_id)	
+);
