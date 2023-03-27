082932c52a68ab3ab724a847ba140bc482dae5a1
RubenABM <rubenbatistamartins@gmail.com>
2023-03-27 14:17:42 +0100

    wip db

--- Database/creates.sql
+++ Database/creates.sql
@@ -1,4 +1,4 @@
-create table user (
+create table users (
 					user_id SERIAL not null,
 					user_name VARCHAR(50) not null,	
                     user_email VARCHAR(50) not null,	
@@ -11,6 +11,7 @@ create table user (
 
 create table students (
 					stu_id SERIAL not null,
+					stu_name VARCHAR(30) not null,		
 					stu_phone VARCHAR(30) not null,						
 					primary key (stu_id)	
 );
