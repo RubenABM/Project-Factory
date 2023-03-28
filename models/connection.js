ede57fa0100af4357e1f52a3c7f0eadaa591b97c
RubenABM <rubenbatistamartins@gmail.com>
2023-03-28 19:56:24 +0100

    wip

--- models/connection.js
+++ models/connection.js
@@ -6,7 +6,7 @@ var pg = require('pg');
 const connectionString = "postgres://postgres:20200453@localhost:5432/testdb";
 //const connectionString = "postgres://" + process.env.DBUSER + ":" + process.env.DBPASS + "@localhost:5432/Products";
 
-console.log("connectionString = " + connectionString);
+console.log("\nconnectionString = " + connectionString);
 
 const Pool = pg.Pool
 const pool = new Pool({
